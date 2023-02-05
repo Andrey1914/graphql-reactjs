22793d84fb8ec1204ba8d33b16718c0afb47605d
Andrey1914 <anlikajw@gmail.com>
2023-02-05 17:28:14 +0100

    added updateMovieMutation at MoviesFormHoc

--- src/components/MoviesForm/MoviesFormHoc.js
+++ src/components/MoviesForm/MoviesFormHoc.js
@@ -2,7 +2,7 @@ import { withStyles } from "@mui/styles";
 import { compose } from "recompose";
 import { graphql } from "react-apollo";
 
-import { addMovieMutation } from "./mutations";
+import { addMovieMutation, updateMovieMutation } from "./mutations";
 import { moviesQuery } from "../MoviesTable/queries";
 import { directorsQuery } from "./queries";
 
@@ -18,9 +18,19 @@ const withGraphqlAdd = graphql(addMovieMutation, {
   }),
 });
 
-// export default compose(styled(styles));
+const withGraphqlupdate = graphql(updateMovieMutation, {
+  props: ({ mutate }) => ({
+    updateMovie: (movie) =>
+      mutate({
+        variables: movie,
+        refetchQueries: [{ query: moviesQuery }],
+      }),
+  }),
+});
+
 export default compose(
   withStyles(styles),
   withGraphqlAdd,
+  withGraphqlupdate,
   graphql(directorsQuery)
 );
