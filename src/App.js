1f7584dbd56489384e3a53f3de3eb354d60ae280
Andrey1914 <anlikajw@gmail.com>
2023-04-30 16:33:02 +0200

    new uri

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
-  uri: `http://localhost:3005/graphql`,
-  // uri: `https://graphql-server-cb11.onrender.com/graphql`,
+  // uri: `http://localhost:3005/graphql`,
+  uri: `https://graphql-server-cb11.onrender.com/graphql`,
   cache: new InMemoryCache(),
 });
 
